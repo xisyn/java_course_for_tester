2d6537c76acb7807812aebb94bdbf9c5bf3f3035
Dmitriy Hitsunov <dkhitsunov@issart.com>
2018-01-10 11:31:29 +0700

    Create IDEA project

--- sandbox/src/main/java/NotFirstHW.java
+++ sandbox/src/main/java/NotFirstHW.java
@@ -1,7 +1,5 @@
 public class NotFirstHW {
-
-		public static void main(String [] args) {
-System.out.println("Hello world!");
-
-		}
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
