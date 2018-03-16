c40d3645f873a4f87144eb0588116935445312e0
Dmitriy Hitsunov <dkhitsunov@issart.com>
2018-03-16 16:40:53 +0700

    Revert "Create IDEA project"
    This reverts commit 2d6537c76acb7807812aebb94bdbf9c5bf3f3035.

--- sandbox/src/main/java/NotFirstHW.java
+++ sandbox/src/main/java/NotFirstHW.java
@@ -1,5 +1,7 @@
 public class NotFirstHW {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+		public static void main(String [] args) {
+System.out.println("Hello world!");
+
+		}
 }
