2bed455ee2bae19ff4c73eff8850740eab72e860
Dmitriy Hitsunov <dkhitsunov@issart.com>
2018-03-20 20:37:25 +0700

    remove psvm in Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/NotFirstHW.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/NotFirstHW.java
@@ -12,10 +12,24 @@ public class NotFirstHW {
         Rectangle r = new Rectangle(4,6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(1,1);
+        Point p3 = new Point(2,2);
+        Point p4 = new Point(4,4);
+
+        System.out.println(distance(p1,p2));
+        System.out.println(distance(p2,p3));
+        System.out.println(distance(p2, p4));
+
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello " + somebody + "!");
+    }
+
+    public static double distance(Point p1, Point p2) {
+        Point p = new Point();
+        return p.distance(p1, p2);
     }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -14,16 +14,8 @@ public class Point {
 
     }
 
-    public double distance (Point p1, Point p2) {
+    public static double distance (Point p1, Point p2) {
         return Math.sqrt(((p2.a - p1.a) * (p2.a - p1.a)) + ((p2.b - p1.b) * (p2.b - p1.b)));
     }
 
-    public static void main(String[] args) {
-        Point p1 = new Point(0,0);
-        Point p2 = new Point(0,0);
-
-        Point p = new Point();
-
-        System.out.println(p.distance(p1,p2));
-    }
 }
